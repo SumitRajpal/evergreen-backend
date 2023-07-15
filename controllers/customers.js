7825a46371cd8f464261ed50f5a8986c4156ab93
SumitRajpal <sumit12rds@gmail.com>
2023-07-16 01:07:43 +0530

    swagger doc for customer added

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,21 +1,23 @@
+const { UUIDV4 } = require("sequelize");
 const { Customers, Customer_Details } = require("../models/customers");
 const { EvergreenTable } = require("../utils/constants");
+const sequelize = require("../utils/database");
 /**
  * @swagger
- * /api/puppies:
+ * /customers:
  *   get:
  *     tags:
- *       - Puppies
- *     description: Returns all puppies
+ *       - All Customers
+ *     description: Returns all customers
  *     produces:
  *       - application/json
  *     responses:
  *       200:
- *         description: An array of puppies
+ *         description: An array of customers
  *         schema:
- *           $ref: '#/definitions/Puppy'
+ *           $ref: '#/definitions/Customers'
  */
-const getCustomer = async (request, response,next) => {
+const getCustomer = async (request, response, next) => {
   try {
     const customers = await Customers.findAndCountAll({
       where: request.body,
@@ -29,7 +31,28 @@ const getCustomer = async (request, response,next) => {
   }
 };
 
-const getCustomerById = async (request, response,next) => {
+/**
+ * @swagger
+ * /customers/{id}:
+ *   get:
+ *     tags:
+ *       - Customer by Id
+ *     description: Returns a single customer
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - id: id
+ *         description: Customers's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single Customer Details
+ *         schema:
+ *           $ref: '#/definitions/Customer'
+ */
+const getCustomerById = async (request, response, next) => {
   const id = request.params.id;
   try {
     const customers = await Customers.findByPk(id, {
@@ -40,12 +63,31 @@ const getCustomerById = async (request, response,next) => {
     });
     response.status(200).json(customers).end();
   } catch (error) {
-    response.status(500).json(error).end();
-    //next(error);
+    next(error);
   }
 };
 
-const setCustomer = async (request, response,next) => {
+/**
+ * @swagger
+ * /customers:
+ *   post:
+ *     tags:
+ *       - Create Customer
+ *     description: Creates a new Customer
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: customer
+ *         description: Customer object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Customer'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+const setCustomer = async (request, response, next) => {
   try {
     const customers = await Customers.create(request.body,
       {
@@ -61,27 +103,27 @@ const setCustomer = async (request, response,next) => {
 
 };
 
-const putCustomer = async (request, response,next) => {
-  if (request.params.id) {
-    try {
-      const customers = await Customers.update(request.body,
-        { where: { id: request.params.id } },
-        {
-          include: [{
-            model: Customer_Details,
-            where: {
-              customer_id: request.params.id
-            },
-            as: "customer_details"
-          }]
-        })
-      response.status(200).json(customers).end();
-    } catch (error) {
-      next(error);
+
+const putCustomer = async (request, response, next) => {
+  try {
+    const transaction = await sequelize.transaction();
+    const customer = await Customers.update(request.body,
+      { where: { id: request.params.id } },
+      { transaction });
+
+    const customer_details = await Customer_Details.update(request.body.customer_details,
+      { where: { customer_id: request.params.id } },
+      { transaction });
+
+    await transaction.commit();
+    response.status(200).json({ message: "Updated Successfully" }).end();
+  } catch (error) {
+    next(error);
+    if (transaction) {
+      await transaction.rollback();
     }
-  } else {
-    response.status(400).json(customers).end();
   }
+
 };
 
 
